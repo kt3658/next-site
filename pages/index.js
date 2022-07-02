29ae9100b6c1c49632cfbd733789dd484996047d
github kt3658 <kt1117tk5@gmail.com>
2022-07-03 08:30:50 +0900

    finished

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import useSWR from 'SWR';
+
+import Header from './components/header';
+import Content from './components/content';
+
+import Styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+
+  let title = "ともすた";
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const {data, error} = useSWR('./api/message',fetcher);
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return (
+    <Content>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Header title={title}/>
+      <p>{data.message}</p>
+      <div>
+        <Link href="/about"><a>About</a></Link>
+      </div>
+      <style jsx>{`
+        h1 {
+          color: white;
+        }
+        `}</style>
+        </Content>
+  )
+}
